4b22a82862cbd83318d79e4de1cef596db456b4f
Eric Brown <ericbrown1.2018@u.northwestern.edu>
2019-12-18 22:53:57 -0500

    Change sma-golden-cross

--- scripts/create_csv.py
+++ scripts/create_csv.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 from coinbase import CoinbasePipeline
 import datetime as dt
 
@@ -8,10 +11,10 @@ days_100 = dt.timedelta(days=100)
 days_150 = dt.timedelta(days=150)
 days_30  = dt.timedelta(days=30)
 month_6  = dt.timedelta(days=180)
-start = dt.datetime.now() - days_100
-pipeline = CoinbasePipeline('ETH-BTC',start=start, granularity=3600)
+start = dt.datetime.now() - one_year
+pipeline = CoinbasePipeline('ETH-BTC',start=start, granularity=60)
 dataframe = pipeline.get_data()
 
 
 dataframe.to_csv(
-r'/Users/ericbrown/code/shelter/exchange-apis/coinbase-api/hist-data/ETH-BTC-100d-1hr-12-16.csv')
+r'/Users/ericbrown/code/shelter/coinbase/hist-data/ETH-BTC-60.csv')
