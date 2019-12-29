6aae3bde80cf777bc17e89e5aba7cbb119955b21
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2019-12-29 15:17:52 -0500

    Add WebsocketHandler class with asynchronous message handlers

--- trading-engine/ticker.py
+++ trading-engine/ticker.py
@@ -5,37 +5,33 @@ from copra.websocket import Channel, Client
 
 ticker_csv = Path("../hist-data") / "BTC-USD_ticker.csv"
 
-tick = {}
 
-async def parse_ticker():
-    global tick
-    while(1):
-        if (not tick):
-            await asyncio.sleep(0)
-        elif(tick):
-            data = pd.Series(tick)
-            tick = {}
-            print(data)
+class WebsocketHandler(object):
+    def __init__(self, message):
+        if message['type'] == 'ticker':
+            loop.create_task(self.handle_ticker(message))
 
-            # data = pd.Series(message, index=['product_id', 'best_bid', 'best_ask', )
-            # data.columns = message.keys()
-            # with open(ticker_csv, mode='a+') as f:
-            #     print(f.tell())
-            #     data.to_csv(f, mode='a+', header=f.tell()==0,)
-            # TODO: to_csv needs proper formatting, series index must become csv column header
+    async def handle_ticker(self, message):
+        await asyncio.sleep(0)
+        data = pd.Series(message)
+        print(data)
+        # data.columns = message.keys()
+        # with open(ticker_csv, mode='a+') as f:
+        #     print(f.tell())
+        #     data.to_csv(f, mode='a+', header=f.tell()==0,)
+        # TODO: to_csv needs proper formatting, series index must become csv column header
 
 
-class Ticker(Client):
+class Websocket(Client):
 
     def on_message(self, message):
-        global tick
-        if message['type'] == 'ticker':
-            tick = message
+        handler = WebsocketHandler(message)
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(parse_ticker())
-    ws_ticker = Ticker(loop, Channel('ticker', 'BTC-USD'))
+    # TODO: listen to multiple channels
+    ws_ticker = Websocket(loop, Channel('ticker', 'BTC-USD'))
 
     try:
         loop.run_forever()
