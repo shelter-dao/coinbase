d3867bed3f178311256bc4df1670877b805005e8
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-01-04 15:57:31 -0500

    Add WebsocketHandler class [wip]

--- trading-engine/websocket.py
+++ trading-engine/websocket.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import pandas as pd
 from datetime import datetime as dt
 from pathlib import Path
@@ -6,49 +7,40 @@ from copra.websocket import Channel, Client
 
 ticker_csv = Path("../hist-data") / "BTC-USD_ticker.csv"
 
+class CandleMaker(object):
+    def __init__(self, data):
+        data
+
 
 class WebsocketHandler(object):
     def __init__(self, message):
-        # TODO: see if this is faster using pythonic switch statement
-        if message['type'] == 'ticker':
-            loop.create_task(self.handle_ticker(message))
-        elif message['type'] == 'matches':
-            loop.create_task(self.handle_matches(message))
+        self.message = message
+        # TODO: see if this is faster using pythonic 'switch' functionality
+        if self.message['type'] == 'ticker':
+            loop.create_task(self.handle_ticker())
+        # elif self.message['type'] == 'matches':
+        #     loop.create_task(self.handle_matches()) etc...
 
-    async def handle_ticker(self, message):
+    async def handle_ticker(self):
         await asyncio.sleep(0)
-        data = pd.DataFrame(message, index=[0])
-        data = data.filter(items=['time', 'product_id', 'best_bid', 'best_ask'])
-        data.time[0] = dt.strptime(data.time[0], '%Y-%m-%dT%H:%M:%S.%fZ')
-        data.rename(columns={"time": "datetime"}, inplace=True)
-        data.set_index('datetime', inplace=True, drop=True)
-        print(data)
-        # data['time'] = dt.fromtimestamp(data['time']) = int(data['time'])
-
-        # data.columns = ['time', 'product_id', 'best_bid', 'best_ask']
-        # data['time'] = dt.fromtimestamp(data['time'])
-        # data.set_index('time', inplace=True, drop=True)
-        # print(data)
-
-        # data.columns = message.keys()
-        # with open(ticker_csv, mode='a+') as f:
-        #     print(f.tell())
-        #     data.to_csv(f, mode='a+', header=f.tell()==0,)
-        # TODO: to_csv needs proper formatting, series index must become csv column header
-
-    # TODO: implement handle_matches()
-    async def handle_matches(self, matches):
-        await asyncio.sleep(0)
-
+        self.data = pd.DataFrame(self.message, index=[0])
+        self.data = self.data.filter(items=['time', 'trade_id', 'product_id', 'best_bid', 'best_ask'])
+        self.data.at[0,'time'] = dt.strptime(self.data.at[0, 'time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        self.data.rename(columns={"time": "datetime"}, inplace=True)
+        self.data.set_index('datetime', inplace=True, drop=True)
+        # print(self.data)
+        # async with open(ticker_csv, mode='a+') as f:
+        #     await data.to_csv(f, mode='a+', header=f.tell()==0,)
 
 class Websocket(Client):
 
     def on_message(self, message):
         handler = WebsocketHandler(message)
 
-
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
+    loop.set_debug(enabled=True)
+    logging.basicConfig(level=logging.DEBUG)
     # TODO: listen to multiple channels
     ws_ticker = Websocket(loop, Channel('ticker', 'BTC-USD'))
 
