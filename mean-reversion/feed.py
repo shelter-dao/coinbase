477fb2723cd209c1cee952ffc5ac15cd7625c5cd
Eric Brown <ericbrown1.2018@u.northwestern.edu>
2020-01-05 07:34:50 +1100

    Feed Testing

--- mean-reversion/feed.py
+++ mean-reversion/feed.py
@@ -4,42 +4,47 @@ import json
 from copra.websocket import Channel, Client
 from autobahn.asyncio.websocket import WebSocketClientFactory
 
-global dataFeed
-
 class MyClient(Client):
 
     def on_message(self, message):
         print(message)
         if message['type'] == 'ticker':
             dataFeed = pd.Series(message)
-            print('**************************************')
-            print(dataFeed)
+            # print('**************************************')
+            # print(dataFeed)
             # print(dataFeed.columns)
             # dataFeed.set_index('time')
             # print(dataFeed)
-            print('**************************************')
+            # print('**************************************')
+            return(dataFeed)
 
-    # TODO: implement a way to open up the feed asynchronously, either as a module or using async.get_event_loop()
-    # def open_feed():
-    #     loop = asyncio.get_event_loop()
-    #     ws1 = MyClient(loop, Channel('ticker', 'BTC-USD'))
-    #
-    #     try:
-    #         loop.run_forever()
-    #     except KeyboardInterrupt:
-    #         # loop.run_until_complete(ws.close())
-    #         loop.run_until_complete(ws1.close())
-    #         loop.close()
+# global dataFeed
+class Feed(object):
+    def __init__(self,)
 
 
 
-loop = asyncio.get_event_loop()
-ws1 = MyClient(loop, Channel('ticker', 'BTC-USD'))
+        # TODO: implement a way to open up the feed asynchronously, either as a module or using async.get_event_loop()
+        # def open_feed():
+        #     loop = asyncio.get_event_loop()
+        #     ws1 = MyClient(loop, Channel('ticker', 'BTC-USD'))
+        #
+        #     try:
+        #         loop.run_forever()
+        #     except KeyboardInterrupt:
+        #         # loop.run_until_complete(ws.close())
+        #         loop.run_until_complete(ws1.close())
+        #         loop.close()
 
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    # loop.run_until_complete(ws.close())
-    loop.run_until_complete(ws1.close())
-    loop.close()
+
+    loop = asyncio.get_event_loop()
+    ws1 = MyClient(loop, Channel('ticker', 'BTC-USD'))
+
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        # loop.run_until_complete(ws.close())
+        loop.run_until_complete(ws1.close())
+        loop.close()
